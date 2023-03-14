7b87d10d6ffd086fb8d8401b3843177199e4caf6
Bwandrius <andriusborkovskij@gmail.com>
2023-03-14 16:48:22 +0200

    comments deleted

--- script.js
+++ script.js
@@ -36,22 +36,3 @@ function cellColourFill () {
 function changeFillColour (choice) {
     colour = choice;
 }
-
-// function makeGrid (rows, cols) {
-
-//     for (let i = 0; i < rows; i++) {
-//         let gridRow = document.createElement("div");
-//         gridRow.classList.add("grid-row");
-
-//         for (let j = 0; j < cols; j++) {
-//             let gridCell = document.createElement("div");
-//             gridCell.classList.add("grid-cell");
-
-//             gridRow.appendChild(gridCell);
-//         }
-
-//         gridContainer.appendChild(gridRow);
-//     }
-// }
-
-// makeGrid(16, 16);
