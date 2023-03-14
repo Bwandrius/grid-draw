bf5c5a4280a11e20422ee18f45f262938884dd18
Bwandrius <andriusborkovskij@gmail.com>
2023-03-14 16:47:41 +0200

    random colour added

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-function fillGrid(size){
+let colour = "black";
+
+function fillGrid (size) {
     let gridContainer = document.querySelector('#grid-container');
     let gridCells = gridContainer.querySelectorAll('div');
     gridCells.forEach(div => div.remove());
     gridContainer.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-    console.log(gridContainer);
 
     let amount = size * size;
 
     for (let i = 0; i < amount; i++) {
         let gridCell = document.createElement('div');
-        gridCell.addEventListener('mouseover', cellColour)
+        gridCell.addEventListener('mouseover', cellColourFill)
         gridCell.style.backgroundColor = 'white';
         gridContainer.insertAdjacentElement('beforeend', gridCell); 
     }
 }
 
-function cellColour (){
-    gridCell.style.backgroundColor = 'black';
-}
-
-function changeSize(input){
-
+function changeSize (input) {
     if (input >= 2 && input <= 100) {
         fillGrid(input);
     } else {
         console.log('invalid input');
     }
+}
 
-    
+function cellColourFill () {
+    if (colour === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = colour;
+    }    
+}
+
+function changeFillColour (choice) {
+    colour = choice;
 }
 
 // function makeGrid (rows, cols) {
